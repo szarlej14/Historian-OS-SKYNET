cb4ead322d4deb8dbe0a0982a9e4d8319c120e25
szarlej14 <florczyk.piotr@gmail.com>
2026-09-02 12:39:19 +0200

    Add Historian OS mega toolkit

---
+++ historianos-toolkit/scripts/graph_viz.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import sys
+root=Path(sys.argv[1] if len(sys.argv)>1 else '.')
+print('Graph export placeholder: records are represented by Obsidian wikilinks and relation fields.')
